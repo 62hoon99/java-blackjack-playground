1149971097ace1ed54b7520b82ca2084c289858f
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-04-15 20:33:12 +0900

    refactor: 다양한 카드 모양 추가

--- src/main/java/nextstep/blackjack/model/CardShape.java
+++ src/main/java/nextstep/blackjack/model/CardShape.java
@@ -1,7 +1,10 @@
 package nextstep.blackjack.model;
 
 public enum CardShape {
-    SPADE("스페이드");
+    SPADE("스페이드"),
+    CLOVER("클로버"),
+    HEART("하트"),
+    DIAMOND("다이아몬드");
 
     private final String kor;
 
--- src/test/java/nextstep/blackjack/model/CardTest.java
+++ src/test/java/nextstep/blackjack/model/CardTest.java
@@ -20,8 +20,14 @@ public class CardTest {
     @Test
     public void 카드는_모양과_이름을_가진다() throws Exception {
         //given
-        Card card = new SingleValueCard(CardShape.SPADE, "4");
+        Card spadeCard = new SingleValueCard(CardShape.SPADE, "4");
+        SingleValueCard cloverCard = new SingleValueCard(CardShape.CLOVER, "5");
+        SingleValueCard heartCard = new SingleValueCard(CardShape.HEART, "K");
+        SingleValueCard diamondCard = new SingleValueCard(CardShape.DIAMOND, "A");
         //then
-        assertThat(card.getInfo()).isEqualTo("4스페이드");
+        assertThat(spadeCard.getInfo()).isEqualTo("4스페이드");
+        assertThat(cloverCard.getInfo()).isEqualTo("5클로버");
+        assertThat(heartCard.getInfo()).isEqualTo("K하트");
+        assertThat(diamondCard.getInfo()).isEqualTo("A다이아몬드");
     }
 }
