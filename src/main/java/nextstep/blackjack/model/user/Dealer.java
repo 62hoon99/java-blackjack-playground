c1e69c2c80cb93777215821aec32dbc01b282e4d
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-04-23 20:20:27 +0900

    feat: 딜러의 카드가 16이하면 카드를 한 장 더 받는 기능 구현

--- src/main/java/nextstep/blackjack/model/user/Dealer.java
+++ src/main/java/nextstep/blackjack/model/user/Dealer.java
@@ -5,7 +5,7 @@ import nextstep.blackjack.model.card.CardPack;
 import nextstep.blackjack.model.card.CardPackFactory;
 
 public class Dealer extends AbstractUser {
-
+    private static final String HIT_MSG = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
     private static final String NAME = "딜러";
     private final CardPack cardPack;
 
@@ -16,5 +16,12 @@ public class Dealer extends AbstractUser {
 
     public Card getCard() {
         return cardPack.getRandomCard();
+    }
+
+    public void hit() {
+        if (getNumber() < 16) {
+            System.out.println(HIT_MSG);
+            receiveCard(getCard());
+        }
     }
 }
