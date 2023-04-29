20ce81b4c53f15c6398e70d8c749de058fc8b2fd
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-04-29 16:16:47 +0900

    feat: stay하면 현 클래스를 반환하는 기능 구현

--- src/test/java/nextstep/blackjack/state/FinishedTest.java
+++ src/test/java/nextstep/blackjack/state/FinishedTest.java
@@ -7,6 +7,8 @@ import nextstep.blackjack.card.Suit;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FinishedTest {
 
     @Test
@@ -30,5 +32,13 @@ public class FinishedTest {
         //then
         Assertions.assertThatThrownBy(() -> blackJack.draw(new Card(Suit.DIAMONDS, Denomination.JACK)))
                 .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    public void stay하면_현재_인스턴스를_그대로_반환() throws Exception {
+        //given
+        Finished stay = new Stay(new Cards());
+        //then
+        assertSame(stay, stay.stay());
     }
 }
